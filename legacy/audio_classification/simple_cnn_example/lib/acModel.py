7df39dee5a9a563d1e31fa4a97c0e575a3fbec29
Andy-Shao <hj13035952619@126.com>
2024-06-02 09:54:30 +0000

    adjust the optimier

--- legacy/audio_classification/simple_cnn_example/lib/acModel.py
+++ legacy/audio_classification/simple_cnn_example/lib/acModel.py
@@ -59,11 +59,14 @@ class AudioClassifier(nn.Module):
 class Processor():
         
     @staticmethod
-    def training(model: nn.Module, train_dl: DataLoader, num_epochs: int, device='cpu'):
-        loss_fn = nn.CrossEntropyLoss().to(device=device)
-        optimizer = optim.SGD(model.parameters(), lr=.1, momentum=.9)
-        scheduler = optim.lr_scheduler.OneCycleLR(optimizer=optimizer, max_lr=.1, steps_per_epoch=int(len(train_dl)), 
-                                              epochs=num_epochs, anneal_strategy='linear')
+    def training(
+        model: nn.Module, train_dl: DataLoader, num_epochs: int, optimizer: optim.Optimizer, loss_fn,
+        device='cpu', silent_mod: bool = False
+    ):
+        # loss_fn = nn.CrossEntropyLoss().to(device=device)
+        # optimizer = optim.SGD(model.parameters(), lr=.1, momentum=.9)
+        # scheduler = optim.lr_scheduler.OneCycleLR(optimizer=optimizer, max_lr=.1, steps_per_epoch=int(len(train_dl)), 
+        #                                       epochs=num_epochs, anneal_strategy='linear')
 
         for epoch in range(num_epochs):
             ttl_loss = .0
@@ -79,7 +82,7 @@ class Processor():
                 loss = loss_fn(outputs, labels)
                 loss.backward()
                 optimizer.step()
-                scheduler.step()
+                # scheduler.step()
 
                 ttl_loss += loss
                 _, pred = torch.max(outputs, dim=1)
@@ -90,8 +93,17 @@ class Processor():
             batch_num = len(train_dl)
             avg_loss = ttl_loss / batch_num
             accuracy = ttl_correct_pred / ttl_pred * 100
-            print(f'Epoch: {epoch}, Loss:{avg_loss:.2f}, accuracy:{accuracy:.2f}%')
-        print('Training End!!')
+            if not silent_mod: 
+                print(f'Epoch: {epoch}, Loss:{avg_loss:.2f}, accuracy:{accuracy:.2f}%')
+        if not silent_mod: print('Training End!!')
+
+    @staticmethod
+    def simple_training(model: nn.Module, train_dl: DataLoader, num_epochs: int, device='cpu', silent_mod: bool = False):
+        loss_fn = nn.CrossEntropyLoss().to(device=device)
+        optimizer = optim.Adam(model.parameters(), lr=.001)
+        Processor.training(model=model, train_dl=train_dl, num_epochs=num_epochs, device=device, loss_fn=loss_fn, 
+            optimizer=optimizer, silent_mod=silent_mod)
+
 
     @staticmethod
     def inference(model: nn.Module, val_dl: DataLoader, device='cpu'):
