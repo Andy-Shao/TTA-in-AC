2482cf9b75c8494da1a58dd5d0ceb8575b5b3026
Andy-Shao <andyshao90@gmail.com>
2024-06-15 17:33:47 +0800

    adjust the shell

--- legacy/test-time-adaptation/SFDA/CoNMix/run.sh
+++
@@ -1,9 +0,0 @@
-sh pre_train.sh
-sleep 30
-
-sh STDA.sh
-sleep 30
-
-sh bridge_MTDA.sh
-sleep 30
-sh MTDA.sh
---
+++ legacy/test-time-adaptation/SFDA/CoNMix/run_clipart.sh
@@ -0,0 +1,4 @@
+python pre_train.py --dataset office-home --model vit --max_epoch 20 --interval 20 --batch_size 64 --source 1 --wandb 1 --output pre_train --trte full
+python STDA.py --source 1 --target 0 2 3 --dataset office-home --net vit --max_epoch 50 --interval 50 --batch_size 32
+python3 bridge_MTDA.py --source 1 --dataset office-home --net vit --batch_size 64
+python3 MTDA.py --dataset office-home --source 1 --batch_size 64 --epoch 100 --interval 5 --net vit --wandb 1
