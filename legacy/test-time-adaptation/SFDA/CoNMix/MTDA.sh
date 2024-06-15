780abff80c987c457812167b0065d8184407b4d2
Andy-Shao <hj13035952619@126.com>
2024-06-15 05:11:47 +0000

    update script

--- legacy/test-time-adaptation/SFDA/CoNMix/MTDA.sh
+++ legacy/test-time-adaptation/SFDA/CoNMix/MTDA.sh
@@ -4,7 +4,7 @@
 # python3 MTDA.py --dset office-home --s 2 --batch_size 64 --epoch 100 --interval 5 --net deit_s --gpu_id 1
 # python3 MTDA.py --dset office-home --s 2 --batch_size 64 --epoch 100 --interval 5 --net deit_s --gpu_id 0
 
-python3 MTDA.py --dataset office-home --source 0 --batch_size 64 --epoch 100 --interval 5 --net vit --wandb 1
-python3 MTDA.py --dataset office-home --source 1 --batch_size 64 --epoch 100 --interval 5 --net vit --wandb 1
-python3 MTDA.py --dataset office-home --source 2 --batch_size 64 --epoch 100 --interval 5 --net vit --wandb 1
-python3 MTDA.py --dataset office-home --source 3 --batch_size 64 --epoch 100 --interval 5 --net vit --wandb 1
+python3 MTDA.py --dataset office-home --source 0 --batch_size 64 --epoch 100 --interval 10 --net vit --wandb 1
+python3 MTDA.py --dataset office-home --source 1 --batch_size 64 --epoch 100 --interval 10 --net vit --wandb 1
+python3 MTDA.py --dataset office-home --source 2 --batch_size 64 --epoch 100 --interval 10 --net vit --wandb 1
+python3 MTDA.py --dataset office-home --source 3 --batch_size 64 --epoch 100 --interval 10 --net vit --wandb 1
--- legacy/test-time-adaptation/SFDA/CoNMix/run.sh
+++ legacy/test-time-adaptation/SFDA/CoNMix/run.sh
@@ -1,6 +1,9 @@
-sh pre_train.sh
+#sh pre_train.sh
+#sleep 30
 
 sh STDA.sh
+sleep 30
 
 sh bridge_MTDA.sh
+sleep 30
 sh MTDA.sh
