454cc52c6d62d7d68d3e387f0bd1b8484596cf03
Andy-Shao <andyshao90@gmail.com>
2024-06-21 01:01:26 +0800

    fix bugs

--- lib/datasets.py
+++ lib/datasets.py
@@ -1,5 +1,14 @@
+
 from torch.utils.data import Dataset
 
 class AudioMINST(Dataset):
-    def __init__(self, data_path: str, is_fair=True):
+    def __init__(self, data_paths: str):
         pass
+
+    def __len__(self):
+        return 0
+    
+    def __getitem__(self, index) -> tuple:
+        audio = 0
+        label = 0
+        return audio, label
