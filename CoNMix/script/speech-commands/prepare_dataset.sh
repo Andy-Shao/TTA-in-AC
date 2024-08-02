1350852aa21b271ba2d7ca8f8fd6e4061012a337
Andy-Shao <hj13035952619@126.com>
2024-08-02 14:15:51 +0000

    add guasian dataset

--- CoNMix/script/speech-commands/prepare_dataset.sh
+++ CoNMix/script/speech-commands/prepare_dataset.sh
@@ -1,3 +1,7 @@
 python -m CoNMix.speech-commands.prepare_corrupted_dataset --dataset_root_path '/home/andyshao/data/speech_commands' \
-    --output_path '/home/andyshao/tmp/speech_commands_10.0' --severity_level 10.0 --corruption 'doing_the_dishes' \
+    --output_path '/home/andyshao/tmp/speech_commands/0.005' --severity_level 0.005 --corruption 'gaussian_noise' \
     --data_type 'final' --cal_strong
+
+# python -m CoNMix.speech-commands.prepare_corrupted_dataset --dataset_root_path '/home/andyshao/data/speech_commands' \
+#     --output_path '/home/andyshao/tmp/speech_commands/bg-10.0' --severity_level 10.0 --corruption 'doing_the_dishes' \
+#     --data_type 'final' --cal_strong
--- CoNMix/speech-commands/prepare_corrupted_dataset.py
+++ CoNMix/speech-commands/prepare_corrupted_dataset.py
@@ -6,10 +6,17 @@ from torchvision import transforms as v_transforms
 from torchaudio import transforms as a_transforms
 
 from lib.toolkit import print_argparse
-from lib.wavUtils import pad_trunc, Components, BackgroundNoise, DoNothing, time_shift
+from lib.wavUtils import pad_trunc, Components, BackgroundNoise, DoNothing, time_shift, GuassianNoise
 from lib.scDataset import SpeechCommandsDataset, BackgroundNoiseDataset
 from lib.datasets import store_to, load_from
 from CoNMix.lib.prepare_dataset import ExpandChannel
+
+def find_background_noise(args: argparse.Namespace) -> tuple[str, torch.Tensor]:
+    background_noise_dataset = BackgroundNoiseDataset(root_path=args.dataset_root_path)
+    for noise_type, noise, _ in background_noise_dataset:
+        if args.corruption == noise_type:
+            return noise_type, noise
+    return ()
 
 if __name__ == '__main__':
     ap = argparse.ArgumentParser()
@@ -18,7 +25,7 @@ if __name__ == '__main__':
     ap.add_argument('--output_path', type=str, default='./result')
     ap.add_argument('--data_type', type=str, choices=['raw', 'final'], default='final')
 
-    ap.add_argument('--corruption', type=str, choices=['doing_the_dishes', 'dude_miaowing', 'exercise_bike', 'pink_noise', 'running_tap', 'white_noise'])
+    ap.add_argument('--corruption', type=str, choices=['doing_the_dishes', 'dude_miaowing', 'exercise_bike', 'pink_noise', 'running_tap', 'white_noise', 'gaussian_noise'])
     ap.add_argument('--severity_level', type=float, default=20)
     # ap.add_argument('--cal_norm', action='store_true')
     ap.add_argument('--cal_strong', action='store_true')
@@ -38,65 +45,72 @@ if __name__ == '__main__':
     hop_length=377
     meta_file_name = 'speech_commands_meta.csv'
 
-    background_noise_dataset = BackgroundNoiseDataset(root_path=args.dataset_root_path)
-    for noise_type, noise, _ in background_noise_dataset:
-        if hasattr(args, 'corruption') and args.corruption != noise_type:
-            continue
-
-        print(f'Generate the corrupted dataset by {noise_type}')
-        output_path = f'{args.output_path}-{noise_type}'
+    if args.corruption == 'gaussian_noise':
+        is_background_noise = False
+        noise_type = args.corruption
+        corrupted_test_tf = Components(transforms=[
+            GuassianNoise(noise_level=args.severity_level),
+            pad_trunc(max_ms=max_ms, sample_rate=sample_rate)
+        ])
+    else:
+        is_background_noise = True
+        noise_type, noise = find_background_noise(args)
         corrupted_test_tf = Components(transforms=[
             BackgroundNoise(noise_level=args.severity_level, noise=noise, is_random=True),
             pad_trunc(max_ms=max_ms, sample_rate=sample_rate),
         ])
-        corrupted_test_dataset = SpeechCommandsDataset(root_path=args.dataset_root_path, mode='test+val', include_rate=False, data_tfs=corrupted_test_tf)
-        store_to(dataset=corrupted_test_dataset, root_path=output_path, index_file_name=meta_file_name)
-        corrupted_test_dataset = load_from(root_path=output_path, index_file_name=meta_file_name)
 
-        print('low augmentation')
-        weak_path = f'{output_path}-weak'
+    print(f'Generate the corrupted dataset by {noise_type}')
+    output_path = f'{args.output_path}-{noise_type}'
+    
+    corrupted_test_dataset = SpeechCommandsDataset(root_path=args.dataset_root_path, mode='test+val', include_rate=False, data_tfs=corrupted_test_tf)
+    store_to(dataset=corrupted_test_dataset, root_path=output_path, index_file_name=meta_file_name)
+    corrupted_test_dataset = load_from(root_path=output_path, index_file_name=meta_file_name)
+
+    print('low augmentation')
+    weak_path = f'{output_path}-weak'
+    if args.data_type == 'raw':
+        weak_tf = DoNothing()
+    elif args.data_type == 'final':
+        weak_tf = Components(transforms=[
+            a_transforms.MelSpectrogram(sample_rate=sample_rate, n_fft=1024, n_mels=n_mels, hop_length=hop_length),
+            a_transforms.AmplitudeToDB(top_db=80),
+            # a_transforms.FrequencyMasking(freq_mask_param=.02),
+            # a_transforms.TimeMasking(time_mask_param=.02),
+            ExpandChannel(out_channel=3),
+            # v_transforms.Resize((256, 256), antialias=False),
+            # v_transforms.RandomCrop(224)
+            v_transforms.Resize((224, 224), antialias=False),
+        ])
+    store_to(dataset=corrupted_test_dataset, root_path=weak_path, index_file_name=meta_file_name, data_transf=weak_tf)
+    weak_aug_dataset = load_from(root_path=weak_path, index_file_name=meta_file_name)
+    print(f'Foreach checking, datasize: {len(weak_aug_dataset)}')
+    for feature, label in tqdm(weak_aug_dataset):
+        pass
+
+    if args.cal_strong:
+        print('strong augmentation')
+        strong_path = f'{output_path}-strong'
         if args.data_type == 'raw':
-            weak_tf = DoNothing()
+            strong_tf = Components(transforms=[
+                a_transforms.PitchShift(sample_rate=sample_rate, n_steps=4, n_fft=512),
+                time_shift(shift_limit=.25, is_random=True, is_bidirection=True)
+            ])
         elif args.data_type == 'final':
-            weak_tf = Components(transforms=[
+            strong_tf = Components(transforms=[
+                a_transforms.PitchShift(sample_rate=sample_rate, n_steps=4, n_fft=512),
+                time_shift(shift_limit=.25, is_random=True, is_bidirection=True),
                 a_transforms.MelSpectrogram(sample_rate=sample_rate, n_fft=1024, n_mels=n_mels, hop_length=hop_length),
                 a_transforms.AmplitudeToDB(top_db=80),
-                # a_transforms.FrequencyMasking(freq_mask_param=.02),
-                # a_transforms.TimeMasking(time_mask_param=.02),
+                # a_transforms.FrequencyMasking(freq_mask_param=.1),
+                # a_transforms.TimeMasking(time_mask_param=.1),
                 ExpandChannel(out_channel=3),
                 # v_transforms.Resize((256, 256), antialias=False),
                 # v_transforms.RandomCrop(224)
                 v_transforms.Resize((224, 224), antialias=False),
             ])
-        store_to(dataset=corrupted_test_dataset, root_path=weak_path, index_file_name=meta_file_name, data_transf=weak_tf)
-        weak_aug_dataset = load_from(root_path=weak_path, index_file_name=meta_file_name)
-        print(f'Foreach checking, datasize: {len(weak_aug_dataset)}')
-        for feature, label in tqdm(weak_aug_dataset):
+        store_to(dataset=corrupted_test_dataset, root_path=strong_path, index_file_name=meta_file_name, data_transf=strong_tf)
+        strong_aug_dataset = load_from(root_path=strong_path, index_file_name=meta_file_name)
+        print(f'Foreach checking, datasize: {len(strong_aug_dataset)}')
+        for feature, label in tqdm(strong_aug_dataset):
             pass
-
-        if args.cal_strong:
-            print('strong augmentation')
-            strong_path = f'{output_path}-strong'
-            if args.data_type == 'raw':
-                strong_tf = Components(transforms=[
-                    a_transforms.PitchShift(sample_rate=sample_rate, n_steps=4, n_fft=512),
-                    time_shift(shift_limit=.25, is_random=True, is_bidirection=True)
-                ])
-            elif args.data_type == 'final':
-                strong_tf = Components(transforms=[
-                    a_transforms.PitchShift(sample_rate=sample_rate, n_steps=4, n_fft=512),
-                    time_shift(shift_limit=.25, is_random=True, is_bidirection=True),
-                    a_transforms.MelSpectrogram(sample_rate=sample_rate, n_fft=1024, n_mels=n_mels, hop_length=hop_length),
-                    a_transforms.AmplitudeToDB(top_db=80),
-                    a_transforms.FrequencyMasking(freq_mask_param=.1),
-                    a_transforms.TimeMasking(time_mask_param=.1),
-                    ExpandChannel(out_channel=3),
-                    # v_transforms.Resize((256, 256), antialias=False),
-                    # v_transforms.RandomCrop(224)
-                    v_transforms.Resize((224, 224), antialias=False),
-                ])
-            store_to(dataset=corrupted_test_dataset, root_path=strong_path, index_file_name=meta_file_name, data_transf=strong_tf)
-            strong_aug_dataset = load_from(root_path=strong_path, index_file_name=meta_file_name)
-            print(f'Foreach checking, datasize: {len(strong_aug_dataset)}')
-            for feature, label in tqdm(strong_aug_dataset):
-                pass
