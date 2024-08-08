bd47c436d4a1faaa24e8c73679ecc230815435c4
Andy-Shao <hj13035952619@126.com>
2024-08-08 10:07:47 +0000

    finish the tent analysis

--- script/speech_commands/pre_train.sh
+++ script/speech_commands/pre_train.sh
@@ -1,8 +1,8 @@
 
 export BASE_PATH='/home/andyshao'
-# python -m tent.speech_commands.pre_train --dataset_root_path $BASE_PATH'/data/speech_commands' --max_epoch 20 \
-#     --model 'cnn' --output_weight_name 'cnn_weights.pt' --wandb
-
 python -m tent.speech_commands.pre_train --dataset_root_path $BASE_PATH'/data/speech_commands' --max_epoch 20 \
-    --model 'restnet50' --output_csv_name 'RestNet50_training_records.csv' \
-    --output_weight_name 'RestNet50_weight.pt' --normalized --wandb 
+    --model 'cnn' --output_weight_name 'cnn_weights.pt' --wandb
+
+# python -m tent.speech_commands.pre_train --dataset_root_path $BASE_PATH'/data/speech_commands' --max_epoch 20 \
+#     --model 'restnet50' --output_csv_name 'RestNet50_training_records.csv' \
+#     --output_weight_name 'RestNet50_weight.pt' --normalized --wandb 
