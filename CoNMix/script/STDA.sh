1c28bf5bc5febe42cfeae6ed8c43619b66e888ee
Andy-Shao <hj13035952619@126.com>
2024-08-17 11:19:53 +0000

    finished the doing the dishes training

--- CoNMix/script/STDA.sh
+++ CoNMix/script/STDA.sh
@@ -21,15 +21,28 @@ export BASE_PATH='/root'
 #     --STDA_modelB_weight_file_name 'audio-mnist_modelB_0025.pt' \
 #     --STDA_modelC_weight_file_name 'audio-mnist_modelC_0025.pt' --wandb --normalized
 
+# python -m CoNMix.STDA --dataset 'audio-mnist' \
+#     --weak_aug_dataset_root_path $BASE_PATH'/tmp/AudioMNIST_analysis/doing_the_dishes/1.0-bg-rand_weak' \
+#     --strong_aug_dataset_root_path $BASE_PATH'/tmp/AudioMNIST_analysis/doing_the_dishes/1.0-bg-rand_strong' \
+#     --batch_size 32 --severity_level 1.0 --max_epoch 50 --interval 50 --lr '1e-4'\
+#     --modelF_weight_path './result/audio-mnist/CoNMix/pre_train/audio-mnist_best_modelF.pt' \
+#     --modelB_weight_path './result/audio-mnist/CoNMix/pre_train/audio-mnist_best_modelB.pt' \
+#     --modelC_weight_path './result/audio-mnist/CoNMix/pre_train/audio-mnist_best_modelC.pt' \
+#     --STDA_modelF_weight_file_name 'audio-mnist_modelF-doing_the_dishes-1.0-bg-rand.pt' \
+#     --STDA_modelB_weight_file_name 'audio-mnist_modelB-doing_the_dishes-1.0-bg-rand.pt' \
+#     --STDA_modelC_weight_file_name 'audio-mnist_modelC-doing_the_dishes-1.0-bg-rand.pt' --wandb --normalized \
+#     --corruption 'doing_the_dishes' --cls_par 0.0 --lr_gamma 30 --fbnm_par 4.0 --cls_mode 'logsoft_nll' \
+#     --plr 0 --const_par 0.6 
+
 python -m CoNMix.STDA --dataset 'audio-mnist' \
-    --weak_aug_dataset_root_path $BASE_PATH'/tmp/AudioMNIST_analysis/doing_the_dishes/1.0-bg-rand_weak' \
-    --strong_aug_dataset_root_path $BASE_PATH'/tmp/AudioMNIST_analysis/doing_the_dishes/1.0-bg-rand_strong' \
-    --batch_size 32 --severity_level 1.0 --max_epoch 50 --interval 50 --lr '1e-4'\
+    --weak_aug_dataset_root_path $BASE_PATH'/tmp/AudioMNIST_analysis/doing_the_dishes/10.0-bg-rand_weak' \
+    --strong_aug_dataset_root_path $BASE_PATH'/tmp/AudioMNIST_analysis/doing_the_dishes/10.0-bg-rand_strong' \
+    --batch_size 32 --severity_level 10.0 --max_epoch 50 --interval 50 --lr '1e-4'\
     --modelF_weight_path './result/audio-mnist/CoNMix/pre_train/audio-mnist_best_modelF.pt' \
     --modelB_weight_path './result/audio-mnist/CoNMix/pre_train/audio-mnist_best_modelB.pt' \
     --modelC_weight_path './result/audio-mnist/CoNMix/pre_train/audio-mnist_best_modelC.pt' \
-    --STDA_modelF_weight_file_name 'audio-mnist_modelF-doing_the_dishes-1.0-bg-rand.pt' \
-    --STDA_modelB_weight_file_name 'audio-mnist_modelB-doing_the_dishes-1.0-bg-rand.pt' \
-    --STDA_modelC_weight_file_name 'audio-mnist_modelC-doing_the_dishes-1.0-bg-rand.pt' --wandb --normalized \
-    --corruption 'doing_the_dishes' --cls_par 0.0 --lr_gamma 30 --fbnm_par 4.0 --cls_mode 'logsoft_nll' \
-    --plr 0 --const_par 0.6 
+    --STDA_modelF_weight_file_name 'audio-mnist_modelF-doing_the_dishes-10.0-bg-rand.pt' \
+    --STDA_modelB_weight_file_name 'audio-mnist_modelB-doing_the_dishes-10.0-bg-rand.pt' \
+    --STDA_modelC_weight_file_name 'audio-mnist_modelC-doing_the_dishes-10.0-bg-rand.pt' --wandb --normalized \
+    --corruption 'doing_the_dishes' --cls_par 1.0 --lr_gamma 10 --fbnm_par 4.0 --cls_mode 'logsoft_ce' \
+    --plr 1 --const_par 0.2
--- CoNMix/script/prepare_dataset.sh
+++ CoNMix/script/prepare_dataset.sh
@@ -9,10 +9,10 @@ export BASE_PATH='/root'
 #     --output_path $BASE_PATH'/tmp/AudioMNIST_analysis_0025' --severity_level 0.0025 --corruption 'gaussian_noise' \
 #     --cal_norm --cal_strong --parallel
 
-python -m CoNMix.prepare_corrupted_dataset --dataset 'audio-mnist' --dataset_root_path $BASE_PATH'/data/AudioMNIST/data' \
-    --output_path $BASE_PATH'/tmp/AudioMNIST_analysis/doing_the_dishes/1.0-bg-rand' --severity_level 1.0 --corruption 'doing_the_dishes' \
-    --background_root_path $BASE_PATH'/data/speech_commands' --cal_strong --parallel --rand_bg
-
 # python -m CoNMix.prepare_corrupted_dataset --dataset 'audio-mnist' --dataset_root_path $BASE_PATH'/data/AudioMNIST/data' \
-#     --output_path $BASE_PATH'/tmp/AudioMNIST_analysis/doing_the_dishes/10.0-bg-rand' --severity_level 10.0 --corruption 'doing_the_dishes' \
+#     --output_path $BASE_PATH'/tmp/AudioMNIST_analysis/doing_the_dishes/1.0-bg-rand' --severity_level 1.0 --corruption 'doing_the_dishes' \
 #     --background_root_path $BASE_PATH'/data/speech_commands' --cal_strong --parallel --rand_bg
+
+python -m CoNMix.prepare_corrupted_dataset --dataset 'audio-mnist' --dataset_root_path $BASE_PATH'/data/AudioMNIST/data' \
+    --output_path $BASE_PATH'/tmp/AudioMNIST_analysis/doing_the_dishes/10.0-bg-rand' --severity_level 10.0 --corruption 'doing_the_dishes' \
+    --background_root_path $BASE_PATH'/data/speech_commands' --cal_strong --parallel --rand_bg
