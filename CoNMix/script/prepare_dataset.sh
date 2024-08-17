c0b18ad12273a80a7f88f10f64130306c53b3c76
Andy-Shao <hj13035952619@126.com>
2024-08-17 09:27:53 +0000

    fix the adjust

--- CoNMix/script/prepare_dataset.sh
+++ CoNMix/script/prepare_dataset.sh
@@ -12,3 +12,7 @@ export BASE_PATH='/root'
 python -m CoNMix.prepare_corrupted_dataset --dataset 'audio-mnist' --dataset_root_path $BASE_PATH'/data/AudioMNIST/data' \
     --output_path $BASE_PATH'/tmp/AudioMNIST_analysis/doing_the_dishes/1.0-bg-rand' --severity_level 1.0 --corruption 'doing_the_dishes' \
     --background_root_path $BASE_PATH'/data/speech_commands' --cal_strong --parallel --rand_bg
+
+# python -m CoNMix.prepare_corrupted_dataset --dataset 'audio-mnist' --dataset_root_path $BASE_PATH'/data/AudioMNIST/data' \
+#     --output_path $BASE_PATH'/tmp/AudioMNIST_analysis/doing_the_dishes/10.0-bg-rand' --severity_level 10.0 --corruption 'doing_the_dishes' \
+#     --background_root_path $BASE_PATH'/data/speech_commands' --cal_strong --parallel --rand_bg
