78662a196bb4cbeb6439fde8f62fb3a9a8a32455
Andy-Shao <hj13035952619@126.com>
2024-08-18 09:01:34 +0000

    adjust code

--- script/analysis.sh
+++ script/analysis.sh
@@ -1,6 +1,6 @@
 # export PYTHONPATH=$PYTHONPATH:$(pwd)
 # export 'PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:256'
-export BASE_PATH='/home/andyshao'
+export BASE_PATH=${BASE_PATH:-'/home/andyshao'}
 
 # python -m tent.analysis --model_weight_file_path './result/audio-mnist/tent/pre_train/model_weights.pt' --dataset_root_path $BASE_PATH'/data/AudioMNIST/data' \
 #     --severity_level 0.0025 --output_csv_name 'accuracy_record_0025.csv'
@@ -25,32 +25,22 @@ export BASE_PATH='/home/andyshao'
 #     --dataset_root_path $BASE_PATH'/data/AudioMNIST/data' --severity_level 0.005 --model 'restnet50' \
 #     --output_csv_name 'RestNet50_batch_accuracy_record_005.csv'
 
-# python -m tent.bg_analysis --model_weight_file_path './result/audio-mnist/tent/pre_train/RestNet50_batch_weight.pt' \
-#     --dataset_root_path $BASE_PATH'/data/AudioMNIST/data' --severity_level 1.0 --model 'restnet50' \
-#     --output_csv_name 'RestNet50_batch_accuracy_record_bg-fixed-1.0.csv' --corruptions 'doing_the_dishes,exercise_bike,running_tap' \
-#     --background_root_path $BASE_PATH'/data/speech_commands'
+python -m tent.bg_analysis --model_weight_file_path './result/audio-mnist/tent/pre_train/RestNet50_batch_weight.pt' \
+    --dataset_root_path $BASE_PATH'/data/AudioMNIST/data' --severity_level 3.0 --model 'restnet50' \
+    --output_csv_name 'RestNet50_batch_accuracy_record_bg-rand-3.0.csv' --corruptions 'doing_the_dishes,exercise_bike,running_tap' \
+    --background_root_path $BASE_PATH'/data/speech_commands' --rand_bg
 
-# python -m tent.bg_analysis --model_weight_file_path './result/audio-mnist/tent/pre_train/RestNet50_batch_weight.pt' \
-#     --dataset_root_path $BASE_PATH'/data/AudioMNIST/data' --severity_level 1.0 --model 'restnet50' \
-#     --output_csv_name 'RestNet50_batch_accuracy_record_bg-rand-1.0.csv' --corruptions 'doing_the_dishes,exercise_bike,running_tap' \
-#     --background_root_path $BASE_PATH'/data/speech_commands' --rand_bg
-
-# python -m tent.bg_analysis --model_weight_file_path './result/audio-mnist/tent/pre_train/model_weights.pt' \
-#     --dataset_root_path $BASE_PATH'/data/AudioMNIST/data' --severity_level 1.0 --model 'cnn' \
-#     --output_csv_name 'cnn_accuracy_record_bg-rand-1.0.csv' --corruptions 'doing_the_dishes,exercise_bike,running_tap' \
-#     --background_root_path $BASE_PATH'/data/speech_commands' --rand_bg
-
-# python -m tent.bg_analysis --model_weight_file_path './result/audio-mnist/tent/pre_train/model_weights.pt' \
-#     --dataset_root_path $BASE_PATH'/data/AudioMNIST/data' --severity_level 1.0 --model 'cnn' \
-#     --output_csv_name 'cnn_accuracy_record_bg-fixed-1.0.csv' --corruptions 'doing_the_dishes,exercise_bike,running_tap' \
-#     --background_root_path $BASE_PATH'/data/speech_commands'
+python -m tent.bg_analysis --model_weight_file_path './result/audio-mnist/tent/pre_train/model_weights.pt' \
+    --dataset_root_path $BASE_PATH'/data/AudioMNIST/data' --severity_level 3.0 --model 'cnn' \
+    --output_csv_name 'cnn_accuracy_record_bg-rand-3.0.csv' --corruptions 'doing_the_dishes,exercise_bike,running_tap' \
+    --background_root_path $BASE_PATH'/data/speech_commands' --rand_bg
 
 # python -m tent.bg_analysis --model_weight_file_path './result/audio-mnist/tent/pre_train/RestNet50_batch_weight.pt' \
 #     --dataset_root_path $BASE_PATH'/data/AudioMNIST/data' --severity_level 10.0 --model 'restnet50' \
 #     --output_csv_name 'RestNet50_batch_accuracy_record_bg-rand-10.0.csv' --corruptions 'doing_the_dishes,exercise_bike,running_tap' \
 #     --background_root_path $BASE_PATH'/data/speech_commands' --rand_bg
 
-python -m tent.bg_analysis --model_weight_file_path './result/audio-mnist/tent/pre_train/model_weights.pt' \
-    --dataset_root_path $BASE_PATH'/data/AudioMNIST/data' --severity_level 10.0 --model 'cnn' \
-    --output_csv_name 'cnn_accuracy_record_bg-rand-10.0.csv' --corruptions 'doing_the_dishes,exercise_bike,running_tap' \
-    --background_root_path $BASE_PATH'/data/speech_commands' --rand_bg
+# python -m tent.bg_analysis --model_weight_file_path './result/audio-mnist/tent/pre_train/model_weights.pt' \
+#     --dataset_root_path $BASE_PATH'/data/AudioMNIST/data' --severity_level 10.0 --model 'cnn' \
+#     --output_csv_name 'cnn_accuracy_record_bg-rand-10.0.csv' --corruptions 'doing_the_dishes,exercise_bike,running_tap' \
+#     --background_root_path $BASE_PATH'/data/speech_commands' --rand_bg
