f14480bdfc10f8e2085d704c628238b2c64b537a
Andy-Shao <hj13035952619@126.com>
2024-08-18 14:52:28 +0000

    add new configs

--- script/speech_commands/analysis.sh
+++ script/speech_commands/analysis.sh
@@ -1,11 +1,16 @@
-export BASE_PATH='/home/andyshao'
+export BASE_PATH=${BASE_PATH:-'/home/andyshao'}
 # python -m tent.speech_commands.analysis --dataset_root_path $BASE_PATH'/data/speech_commands' --model 'cnn' \
 #     --severity_level 0.005 --output_csv_name 'accuracy_record_cnn_gaussian_noise_005.csv' \
 #     --model_weight_file_path './result/speech-commands/tent/pre_train/cnn_weights.pt' \
 #     --corruptions 'gaussian_noise'
 
 python -m tent.speech_commands.analysis --dataset_root_path $BASE_PATH'/data/speech_commands' --model 'cnn' \
-    --severity_level 1.0 --output_csv_name 'accuracy_record_cnn_background_1.0.csv' \
+    --severity_level 10.0 --output_csv_name 'accuracy_record_cnn_background_10.0.csv' \
+    --model_weight_file_path './result/speech-commands/tent/pre_train/cnn_weights.pt' \
+    --corruptions 'doing_the_dishes,exercise_bike,running_tap'
+
+python -m tent.speech_commands.analysis --dataset_root_path $BASE_PATH'/data/speech_commands' --model 'cnn' \
+    --severity_level 3.0 --output_csv_name 'accuracy_record_cnn_background_3.0.csv' \
     --model_weight_file_path './result/speech-commands/tent/pre_train/cnn_weights.pt' \
     --corruptions 'doing_the_dishes,exercise_bike,running_tap'
 
@@ -14,7 +19,12 @@ python -m tent.speech_commands.analysis --dataset_root_path $BASE_PATH'/data/spe
 #     --model_weight_file_path './result/speech-commands/tent/pre_train/RestNet50_weight.pt' --normalized \
 #     --corruptions 'gaussian_noise'
 
-# python -m tent.speech_commands.analysis --dataset_root_path $BASE_PATH'/data/speech_commands' --model 'restnet50' \
-#     --severity_level 1.0 --output_csv_name 'accuracy_record_RestNet50_background_1.0.csv' \
-#     --model_weight_file_path './result/speech-commands/tent/pre_train/RestNet50_weight.pt' --normalized \
-#     --corruptions 'doing_the_dishes,exercise_bike,running_tap'
+python -m tent.speech_commands.analysis --dataset_root_path $BASE_PATH'/data/speech_commands' --model 'restnet50' \
+    --severity_level 10.0 --output_csv_name 'accuracy_record_RestNet50_background_10.0.csv' \
+    --model_weight_file_path './result/speech-commands/tent/pre_train/RestNet50_weight.pt' --normalized \
+    --corruptions 'doing_the_dishes,exercise_bike,running_tap'
+
+python -m tent.speech_commands.analysis --dataset_root_path $BASE_PATH'/data/speech_commands' --model 'restnet50' \
+    --severity_level 3.0 --output_csv_name 'accuracy_record_RestNet50_background_3.0.csv' \
+    --model_weight_file_path './result/speech-commands/tent/pre_train/RestNet50_weight.pt' --normalized \
+    --corruptions 'doing_the_dishes,exercise_bike,running_tap'
--- script/speech_commands/ttt_analysis.sh
+++ script/speech_commands/ttt_analysis.sh
@@ -1,4 +1,4 @@
-export BASE_PATH='/root'
+export BASE_PATH=${BASE_PATH:-'/root'}
 
 # python -m ttt.speech_commands.time_shift_analysis \
 #     --origin_model_weight_file_path './result/speech-commands/ttt/pre_time_shift_train/ts_bn_ckpt.pth' \
@@ -8,8 +8,15 @@ export BASE_PATH='/root'
 
 python -m ttt.speech_commands.time_shift_analysis \
     --origin_model_weight_file_path './result/speech-commands/ttt/pre_time_shift_train/ts_bn_ckpt.pth' \
-    --dataset 'speech-commands' --output_csv_name 'background_1.0-accuracy_record.csv' \
-    --dataset_root_path $BASE_PATH'/data/speech_commands' --depth 50 --severity_level 1.0 --batch_size 126 \
+    --dataset 'speech-commands' --output_csv_name 'background_3.0-accuracy_record.csv' \
+    --dataset_root_path $BASE_PATH'/data/speech_commands' --depth 50 --severity_level 3.0 --batch_size 126 \
+    --corruptions 'doing_the_dishes,exercise_bike,running_tap' \
+    --TTT_analysis --lr 1e-3 --threshold 0.99 --shift_limit 0.2625
+
+python -m ttt.speech_commands.time_shift_analysis \
+    --origin_model_weight_file_path './result/speech-commands/ttt/pre_time_shift_train/ts_bn_ckpt.pth' \
+    --dataset 'speech-commands' --output_csv_name 'background_10.0-accuracy_record.csv' \
+    --dataset_root_path $BASE_PATH'/data/speech_commands' --depth 50 --severity_level 10.0 --batch_size 126 \
     --corruptions 'doing_the_dishes,exercise_bike,running_tap' \
     --TTT_analysis --lr 1e-3 --threshold 0.99 --shift_limit 0.2625
 
