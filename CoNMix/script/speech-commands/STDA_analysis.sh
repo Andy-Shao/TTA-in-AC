3599fc0d5ff236914914a3ad411019c7d5b3be3d
Andy-Shao <hj13035952619@126.com>
2024-08-20 11:28:12 +0000

    record

--- CoNMix/script/speech-commands/STDA_analysis.sh
+++ CoNMix/script/speech-commands/STDA_analysis.sh
@@ -52,27 +52,27 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 #     --normalized --severity_level 3.0 --data_type 'final' --corruption 'running_tap' \
 #     --dataset 'speech-commands-purity'
 
-python -m CoNMix.speech-commands.STDA_analysis --dataset_root_path $BASE_PATH'/data/speech_commands' \
-    --temporary_path $BASE_PATH'/tmp/speech_commands/doing_the_dishes-bg/10.0-doing_the_dishes-weak' \
-    --modelF_weight_path './result/speech-commands/CoNMix/pre_train/speech-commands_best_modelF.pt' \
-    --modelB_weight_path './result/speech-commands/CoNMix/pre_train/speech-commands_best_modelB.pt' \
-    --modelC_weight_path './result/speech-commands/CoNMix/pre_train/speech-commands_best_modelC.pt' \
-    --adapted_modelF_weight_path './result/speech-commands/CoNMix/STDA/speech-commands_modelF-bg-10.0-doing_the_dishes.pt' \
-    --adapted_modelB_weight_path './result/speech-commands/CoNMix/STDA/speech-commands_modelB-bg-10.0-doing_the_dishes.pt' \
-    --adapted_modelC_weight_path './result/speech-commands/CoNMix/STDA/speech-commands_modelC-bg-10.0-doing_the_dishes.pt' \
-    --normalized --severity_level 10.0 --data_type 'final' --corruption 'doing_the_dishes' \
-    --dataset 'speech-commands' --output_csv_name 'bg-10.0-doing_the_dishes_accuracy_record.csv'
-
 # python -m CoNMix.speech-commands.STDA_analysis --dataset_root_path $BASE_PATH'/data/speech_commands' \
-#     --temporary_path $BASE_PATH'/tmp/speech_commands/exercise_bike-bg/10.0-exercise_bike-weak' \
+#     --temporary_path $BASE_PATH'/tmp/speech_commands/doing_the_dishes-bg/10.0-doing_the_dishes-weak' \
 #     --modelF_weight_path './result/speech-commands/CoNMix/pre_train/speech-commands_best_modelF.pt' \
 #     --modelB_weight_path './result/speech-commands/CoNMix/pre_train/speech-commands_best_modelB.pt' \
 #     --modelC_weight_path './result/speech-commands/CoNMix/pre_train/speech-commands_best_modelC.pt' \
-#     --adapted_modelF_weight_path './result/speech-commands/CoNMix/STDA/speech-commands_modelF-bg-10.0-exercise_bike.pt' \
-#     --adapted_modelB_weight_path './result/speech-commands/CoNMix/STDA/speech-commands_modelB-bg-10.0-exercise_bike.pt' \
-#     --adapted_modelC_weight_path './result/speech-commands/CoNMix/STDA/speech-commands_modelC-bg-10.0-exercise_bike.pt' \
-#     --normalized --severity_level 10.0 --data_type 'final' --corruption 'exercise_bike' \
-#     --dataset 'speech-commands' --output_csv_name 'bg-10.0-exercise_bike_accuracy_record.csv' --analyze_STDA
+#     --adapted_modelF_weight_path './result/speech-commands/CoNMix/STDA/speech-commands_modelF-bg-10.0-doing_the_dishes.pt' \
+#     --adapted_modelB_weight_path './result/speech-commands/CoNMix/STDA/speech-commands_modelB-bg-10.0-doing_the_dishes.pt' \
+#     --adapted_modelC_weight_path './result/speech-commands/CoNMix/STDA/speech-commands_modelC-bg-10.0-doing_the_dishes.pt' \
+#     --normalized --severity_level 10.0 --data_type 'final' --corruption 'doing_the_dishes' \
+#     --dataset 'speech-commands' --output_csv_name 'bg-10.0-doing_the_dishes_accuracy_record.csv'
+
+python -m CoNMix.speech-commands.STDA_analysis --dataset_root_path $BASE_PATH'/data/speech_commands' \
+    --temporary_path $BASE_PATH'/tmp/speech_commands/exercise_bike-bg/10.0-exercise_bike-weak' \
+    --modelF_weight_path './result/speech-commands/CoNMix/pre_train/speech-commands_best_modelF.pt' \
+    --modelB_weight_path './result/speech-commands/CoNMix/pre_train/speech-commands_best_modelB.pt' \
+    --modelC_weight_path './result/speech-commands/CoNMix/pre_train/speech-commands_best_modelC.pt' \
+    --adapted_modelF_weight_path './result/speech-commands/CoNMix/STDA/speech-commands_modelF-bg-10.0-exercise_bike.pt' \
+    --adapted_modelB_weight_path './result/speech-commands/CoNMix/STDA/speech-commands_modelB-bg-10.0-exercise_bike.pt' \
+    --adapted_modelC_weight_path './result/speech-commands/CoNMix/STDA/speech-commands_modelC-bg-10.0-exercise_bike.pt' \
+    --normalized --severity_level 10.0 --data_type 'final' --corruption 'exercise_bike' \
+    --dataset 'speech-commands' --output_csv_name 'bg-10.0-exercise_bike_accuracy_record.csv' --analyze_STDA
     
 # python -m CoNMix.speech-commands.STDA_analysis --dataset_root_path $BASE_PATH'/data/speech_commands' \
 #     --temporary_path $BASE_PATH'/tmp/speech_commands/running_tap-bg/10.0-running_tap-weak' \
--- CoNMix/script/speech-commands/prepare_dataset.sh
+++ CoNMix/script/speech-commands/prepare_dataset.sh
@@ -25,13 +25,13 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 #     --output_path $BASE_PATH'/tmp/speech_commands_purity/running_tap-bg_fixed/3.0' --severity_level 3.0 --corruption 'running_tap' \
 #     --data_type 'final' --cal_strong --dataset 'speech-commands-purity' --parallel
 
-python -m CoNMix.speech-commands.prepare_corrupted_dataset --dataset_root_path $BASE_PATH'/data/speech_commands' \
-    --output_path $BASE_PATH'/tmp/speech_commands/doing_the_dishes-bg/10.0' --severity_level 10.0 --corruption 'doing_the_dishes' \
-    --data_type 'final' --cal_strong --dataset 'speech-commands' --parallel --rand_bg
-
 # python -m CoNMix.speech-commands.prepare_corrupted_dataset --dataset_root_path $BASE_PATH'/data/speech_commands' \
-#     --output_path $BASE_PATH'/tmp/speech_commands/exercise_bike-bg/10.0' --severity_level 10.0 --corruption 'exercise_bike' \
+#     --output_path $BASE_PATH'/tmp/speech_commands/doing_the_dishes-bg/10.0' --severity_level 10.0 --corruption 'doing_the_dishes' \
 #     --data_type 'final' --cal_strong --dataset 'speech-commands' --parallel --rand_bg
+
+python -m CoNMix.speech-commands.prepare_corrupted_dataset --dataset_root_path $BASE_PATH'/data/speech_commands' \
+    --output_path $BASE_PATH'/tmp/speech_commands/exercise_bike-bg/10.0' --severity_level 10.0 --corruption 'exercise_bike' \
+    --data_type 'final' --cal_strong --dataset 'speech-commands' --parallel --rand_bg
 
 # python -m CoNMix.speech-commands.prepare_corrupted_dataset --dataset_root_path $BASE_PATH'/data/speech_commands' \
 #     --output_path $BASE_PATH'/tmp/speech_commands/running_tap-bg/10.0' --severity_level 10.0 --corruption 'running_tap' \
