4a65d4892a889764b3fd8556e236776a8db3ba52
Andy-Shao <hj13035952619@126.com>
2024-08-29 17:58:35 +0000

    finish the ttt testing

--- script/speech_commands/ttt_analysis.sh
+++ script/speech_commands/ttt_analysis.sh
@@ -26,16 +26,16 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 #     --dataset_root_path $BASE_PATH'/data/speech_commands' --depth 50 --severity_level 0.005 --batch_size 96 \
 #     --corruptions 'gaussian_noise' --normalized
 
-# python -m ttt.speech_commands.time_shift_analysis \
-#     --origin_model_weight_file_path './result/speech-commands-random/ttt/pre_time_shift_train/ts_bn_ckpt.pth' \
-#     --dataset 'speech-commands-random' --output_csv_name 'background_10.0-accuracy_record.csv' \
-#     --dataset_root_path $BASE_PATH'/data/speech_commands' --depth 50 --severity_level 10.0 --batch_size 126 \
-#     --corruptions 'doing_the_dishes,exercise_bike,running_tap' \
-#     --TTT_analysis --lr 1e-3 --threshold 0.99 --shift_limit 0.2625
-
 python -m ttt.speech_commands.time_shift_analysis \
     --origin_model_weight_file_path './result/speech-commands-random/ttt/pre_time_shift_train/ts_bn_ckpt.pth' \
-    --dataset 'speech-commands-random' --output_csv_name 'background_3.0-accuracy_record.csv' \
-    --dataset_root_path $BASE_PATH'/data/speech_commands' --depth 50 --severity_level 3.0 --batch_size 126 \
+    --dataset 'speech-commands-random' --output_csv_name 'background_10.0-accuracy_record.csv' \
+    --dataset_root_path $BASE_PATH'/data/speech_commands' --depth 50 --severity_level 10.0 --batch_size 126 \
     --corruptions 'doing_the_dishes,exercise_bike,running_tap' \
     --TTT_analysis --lr 1e-3 --threshold 0.99 --shift_limit 0.2625
+
+# python -m ttt.speech_commands.time_shift_analysis \
+#     --origin_model_weight_file_path './result/speech-commands-random/ttt/pre_time_shift_train/ts_bn_ckpt.pth' \
+#     --dataset 'speech-commands-random' --output_csv_name 'background_3.0-accuracy_record.csv' \
+#     --dataset_root_path $BASE_PATH'/data/speech_commands' --depth 50 --severity_level 3.0 --batch_size 126 \
+#     --corruptions 'doing_the_dishes,exercise_bike,running_tap' \
+#     --TTT_analysis --lr 1e-3 --threshold 0.99 --shift_limit 0.2625
