c22fb78e3e0b8ebb45c3d061d0ea05dac17f9d00
Andy-Shao <hj13035952619@126.com>
2024-09-30 08:34:13 +0000

    fix bugs

--- CoNMix/script/speech-commands/pre_train.sh
+++ CoNMix/script/speech-commands/pre_train.sh
@@ -5,11 +5,12 @@ export BASE_PATH='/root'
 #     --normalized --dataset 'speech-commands' --output_weight_prefix 'speech-commands' \
 #     --num_workers 16 --wandb
 
-# python -m CoNMix.speech-commands.pre_train --dataset_root_path $BASE_PATH'/data/speech_commands' --max_epoch 10 \
-#     --interval 20 --batch_size 64 --trte full --temporary_path $BASE_PATH'/tmp/speech_commands_purity_train' \
-#     --normalized --dataset 'speech-commands-purity' --output_weight_prefix 'speech-commands-purity' --wandb
+# python -m CoNMix.speech-commands.pre_train --dataset_root_path $BASE_PATH'/data/speech_commands' --max_epoch 20 \
+#     --interval 20 --batch_size 64 --trte full --temporary_path $BASE_PATH'/tmp/speech_commands_train' \
+#     --normalized --dataset 'speech-commands-random' --output_weight_prefix 'speech-commands' \
+#     --num_workers 16 --wandb
 
 python -m CoNMix.speech-commands.pre_train --dataset_root_path $BASE_PATH'/data/speech_commands' --max_epoch 20 \
     --interval 20 --batch_size 64 --trte full --temporary_path $BASE_PATH'/tmp/speech_commands_train' \
-    --normalized --dataset 'speech-commands-random' --output_weight_prefix 'speech-commands' \
+    --normalized --dataset 'speech-commands-numbers' --output_weight_prefix 'speech-commands-numbers' \
     --num_workers 16 --wandb
