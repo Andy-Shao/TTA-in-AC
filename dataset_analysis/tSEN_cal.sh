69fe28f0fabc767b92649c8d2a0fd47bdbc47f46
Andy-Shao <andyshao90@gmail.com>
2024-10-12 20:53:32 +0800

    adjut the shell;

--- dataset_analysis/tSEN_cal.sh
+++ dataset_analysis/tSEN_cal.sh
@@ -1,15 +1,15 @@
 export BASE_PATH=${BASE_PATH:-'\root'}
 
-# python -m dataset_analysis.tSEN_calculation --dataset_root_path $BASE_PATH'/data/AudioMNIST/data' \
-#     --dataset 'audio-mnist' --output_file 'audio-mnist_full_tsen.csv' --mode 'full'
-
-# python -m dataset_analysis.tSEN_calculation --dataset_root_path $BASE_PATH'/data/speech_commands' \
-#     --dataset 'speech-commands' --output_file 'speech-commands_full_tsen.csv' --mode 'full'
-
 python -m dataset_analysis.tSEN_calculation --dataset_root_path $BASE_PATH'/data/AudioMNIST/data' \
-    --dataset 'audio-mnist' --output_file 'audio-mnist_full_noreduced_tsen.csv' --mode 'full' \
-    --no_reduce
+    --dataset 'audio-mnist' --output_file 'audio-mnist_full_tsen.csv' --mode 'full'
 
 python -m dataset_analysis.tSEN_calculation --dataset_root_path $BASE_PATH'/data/speech_commands' \
-    --dataset 'speech-commands' --output_file 'speech-commands_full_noreduced_tsen.csv' --mode 'full' \
-    --no_reduce
+    --dataset 'speech-commands' --output_file 'speech-commands_full_tsen.csv' --mode 'full'
+
+# python -m dataset_analysis.tSEN_calculation --dataset_root_path $BASE_PATH'/data/AudioMNIST/data' \
+#     --dataset 'audio-mnist' --output_file 'audio-mnist_full_noreduced_tsen.csv' --mode 'full' \
+#     --no_reduce
+
+# python -m dataset_analysis.tSEN_calculation --dataset_root_path $BASE_PATH'/data/speech_commands' \
+#     --dataset 'speech-commands' --output_file 'speech-commands_full_noreduced_tsen.csv' --mode 'full' \
+#     --no_reduce
