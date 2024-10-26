cf81b0ede443456ba7fc31e1ffd52c7dd08f4e6b
Andy-Shao <hj13035952619@126.com>
2024-10-26 07:22:44 +0000

    adjust the code

--- script/speech_commands/ttt_analysis.sh
+++ script/speech_commands/ttt_analysis.sh
@@ -13,12 +13,12 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 #     --corruptions 'doing_the_dishes,exercise_bike,running_tap' \
 #     --TTT_analysis --lr 1e-3 --threshold 0.99 --shift_limit 0.2625
 
-# python -m ttt.speech_commands.time_shift_analysis \
-#     --origin_model_weight_file_path './result/speech-commands/ttt/pre_time_shift_train/ts_bn_ckpt.pth' \
-#     --dataset 'speech-commands' --output_csv_name 'background_3.0-accuracy_record-offline.csv' \
-#     --dataset_root_path $BASE_PATH'/data/speech_commands' --depth 26 --severity_level 3.0 --batch_size 32 \
-#     --corruptions 'exercise_bike' \
-#     --TTT_analysis --lr 1e-3 --threshold 0.99 --shift_limit 0.2625 --mode 'offline' --niter 20
+python -m ttt.speech_commands.time_shift_analysis \
+    --origin_model_weight_file_path './result/speech-commands/ttt/pre_time_shift_train/ts_bn_ckpt.pth' \
+    --dataset 'speech-commands' --output_csv_name 'background_3.0-accuracy_record-offline.csv' \
+    --dataset_root_path $BASE_PATH'/data/speech_commands' --depth 26 --severity_level 3.0 --batch_size 126 \
+    --corruptions 'doing_the_dishes,exercise_bike,running_tap' \
+    --TTT_analysis --lr 1e-3 --threshold 0.99 --shift_limit 0.2625 --mode 'offline' --niter 10
 
 # python -m ttt.speech_commands.time_shift_analysis \
 #     --origin_model_weight_file_path './result/speech-commands/ttt/pre_time_shift_train/ts_bn_ckpt.pth' \
@@ -30,9 +30,9 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 python -m ttt.speech_commands.time_shift_analysis \
     --origin_model_weight_file_path './result/speech-commands/ttt/pre_time_shift_train/ts_bn_ckpt.pth' \
     --dataset 'speech-commands' --output_csv_name 'background_10.0-accuracy_record-offline.csv' \
-    --dataset_root_path $BASE_PATH'/data/speech_commands' --depth 26 --severity_level 10.0 --batch_size 32 \
-    --corruptions 'exercise_bike' \
-    --TTT_analysis --lr 1e-3 --threshold 0.99 --shift_limit 0.2625 --mode 'offline' --niter 20
+    --dataset_root_path $BASE_PATH'/data/speech_commands' --depth 26 --severity_level 10.0 --batch_size 126 \
+    --corruptions 'doing_the_dishes,exercise_bike,running_tap' \
+    --TTT_analysis --lr 1e-3 --threshold 0.99 --shift_limit 0.2625 --mode 'offline' --niter 10
 
 # python -m ttt.speech_commands.time_shift_analysis \
 #     --origin_model_weight_file_path './result/speech-commands-random/ttt/pre_time_shift_train/ts_bn_ckpt.pth' \
