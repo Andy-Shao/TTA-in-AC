719d152886c5e9df12000a6bbcbeead347ebac30
Andy-Shao <hj13035952619@126.com>
2025-01-01 13:09:59 +0000

    abl study

--- CoNMix/script/speech-commands/prepare_dataset.sh
+++ CoNMix/script/speech-commands/prepare_dataset.sh
@@ -5,17 +5,17 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 #     --output_path $BASE_PATH'/tmp/speech_commands/guassian_noise/0.005' --severity_level 0.005 --corruption 'guassian_noise' \
 #     --data_type 'final' --cal_strong --dataset 'speech-commands' --parallel
 
-python -m CoNMix.speech-commands.prepare_corrupted_dataset --dataset_root_path $BASE_PATH'/data/speech_commands' \
-    --output_path $BASE_PATH'/tmp/speech_commands/doing_the_dishes-bg/3.0' --severity_level 3.0 --corruption 'doing_the_dishes' \
-    --data_type 'final' --cal_strong --dataset 'speech-commands' --parallel --rand_bg
+# python -m CoNMix.speech-commands.prepare_corrupted_dataset --dataset_root_path $BASE_PATH'/data/speech_commands' \
+#     --output_path $BASE_PATH'/tmp/speech_commands/doing_the_dishes-bg/3.0' --severity_level 3.0 --corruption 'doing_the_dishes' \
+#     --data_type 'final' --cal_strong --dataset 'speech-commands' --parallel --rand_bg
 
 python -m CoNMix.speech-commands.prepare_corrupted_dataset --dataset_root_path $BASE_PATH'/data/speech_commands' \
     --output_path $BASE_PATH'/tmp/speech_commands/exercise_bike-bg/3.0' --severity_level 3.0 --corruption 'exercise_bike' \
     --data_type 'final' --cal_strong --dataset 'speech-commands' --parallel --rand_bg
 
-# python -m CoNMix.speech-commands.prepare_corrupted_dataset --dataset_root_path $BASE_PATH'/data/speech_commands' \
-#     --output_path $BASE_PATH'/tmp/speech_commands/running_tap-bg/3.0' --severity_level 3.0 --corruption 'running_tap' \
-#     --data_type 'final' --cal_strong --dataset 'speech-commands' --parallel --rand_bg
+python -m CoNMix.speech-commands.prepare_corrupted_dataset --dataset_root_path $BASE_PATH'/data/speech_commands' \
+    --output_path $BASE_PATH'/tmp/speech_commands/running_tap-bg/3.0' --severity_level 3.0 --corruption 'running_tap' \
+    --data_type 'final' --cal_strong --dataset 'speech-commands' --parallel --rand_bg
 
 # python -m CoNMix.speech-commands.prepare_corrupted_dataset --dataset_root_path $BASE_PATH'/data/speech_commands' \
 #     --output_path $BASE_PATH'/tmp/speech_commands_purity/running_tap-bg/3.0' --severity_level 3.0 --corruption 'running_tap' \
