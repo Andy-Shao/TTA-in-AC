b88163233d04a1c87fca8a11542d13a160804515
Andy-Shao <hj13035952619@126.com>
2025-07-13 09:51:04 +0000

    fix bugs

--- CoNMix/script/pre_train.sh
+++ CoNMix/script/pre_train.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 export BASE_PATH=${BASE_PATH:-'/root'}
 
-python CoNMix/pre_train.py --dataset_root_path $BASE_PATH'/data/AudioMNIST/data' --max_epoch 20 \
+python -m CoNMix.pre_train --dataset_root_path $BASE_PATH'/data/AudioMNIST/data' --max_epoch 20 \
     --interval 20 --batch_size 64 --trte full --temporary_path $BASE_PATH'/tmp/AudioMNIST_train' \
     --wandb --test_rate 0.1
